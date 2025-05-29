e16dc0e80ec26062f94cdb861a663b5deadb2e33
Roman <astek14@mail.ru>
2025-05-29 16:44:30 +0300

    week3 add hint

--- src/main/java/cheatsheet/completableFuture/Task4.java
+++ src/main/java/cheatsheet/completableFuture/Task4.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CompletableFuture;
 public class Task4 {
     public static void main(String[] args) {
         // Interface CompletionStage
+        // Например, thenApply(), thenCompose() и другие методы CompletableFuture являются частью интерфейса CompletionStage
+        // и позволяют строить последовательные цепочки задач, где результат одного этапа используется в следующем.
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Привет")
                 .thenApply(greeting -> greeting + ", мир!")
                 .thenApply(fullGreeting -> fullGreeting + " Как дела?");
