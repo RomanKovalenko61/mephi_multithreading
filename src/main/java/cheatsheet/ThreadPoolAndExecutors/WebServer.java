ae7e1b00b314d77f518db918a1cc25d87e36e30b
Roman <astek14@mail.ru>
2025-05-29 15:23:13 +0300

    week3 add custom threadPool

---
+++ src/main/java/cheatsheet/ThreadPoolAndExecutors/WebServer.java
@@ -0,0 +1,32 @@
+package cheatsheet.ThreadPoolAndExecutors;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class WebServer {
+    public static void main(String[] args) {
+        int corePoolSize = 10;
+        int maximumPoolSize = 50;
+        long keepAliveTime = 30;
+        TimeUnit unit = TimeUnit.SECONDS;
+        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(100);
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + " is processing a request");
+                // Имитация обработки запроса
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+        executor.shutdown();
+    }
+}
